526c9085b396a8a44c5bea510a0554cfcc38b5ab
shadow_walker <shadowwalkersb@gmail.com>
2017-05-15 14:27:29 -0500

    customize conda-forge.yml

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -13,8 +13,9 @@ docker info
 config=$(cat <<CONDARC
 
 channels:
- - conda-forge
+ - cryoem
  - defaults
+ - conda-forge
 
 conda-build:
  root-dir: /feedstock_root/build_artefacts
@@ -58,6 +59,9 @@ conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
 # Embarking on 1 case(s).
+    set -x
+    export CONDA_PY=27
+    set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 touch /feedstock_root/build_artefacts/conda-forge-build-done
