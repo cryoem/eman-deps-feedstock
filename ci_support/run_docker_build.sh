4bd63a0c1bdd6bb3e6395f67b29c195bd30913ca
shadow_walker <shadowwalkersb@gmail.com>
2017-05-15 21:37:53 -0500

    set +e

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -58,6 +58,8 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
+set +e
+
 # Embarking on 6 case(s).
     set -x
     export CONDA_NPY=17
