e14099bbab5f90d6a851f6030566b4ad97f8043c
shadow_walker <shadowwalkersb@gmail.com>
2021-01-27 16:38:52 -0600

    separate pydusa imports

--- recipe/test_imports.py
+++ recipe/test_imports.py
@@ -36,16 +36,6 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.backends.backend_pdf import FigureCanvasPdf
 from matplotlib.figure import Figure
 from matplotlib.ticker import MaxNLocator
-from mpi import MPI_CHAR
-from mpi import MPI_COMM_WORLD
-from mpi import mpi_bcast
-from mpi import mpi_comm_rank
-from mpi import mpi_finalize
-from mpi import mpi_get_count
-from mpi import mpi_init
-from mpi import mpi_probe
-from mpi import mpi_recv
-from mpi import mpi_send
 from mpl_toolkits.mplot3d import Axes3D
 from multiprocessing import Array
 from multiprocessing import Pool
